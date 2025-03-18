6b9b5fc5a6800de583953e664668613500d49787
Борислав Перепелицин <borislav3001@gmail.com>
2025-03-18 20:41:32 +0100

    xpath locators 1st part

---
+++ lesson_7/locators.py
@@ -0,0 +1,14 @@
+#Logo
+LOGO = ("xpath", "(//div//a/img)[1]")
+
+#explore pop-up menu
+POPUP = ("xpath", "//div[contains(@class, 'dropdown_toggle')]")
+
+#slider
+SLIDER = ("xpath", "//div([contains(@class, 'slider_track')] and @role, 'slider')")
+
+#card
+CARD = ("xpath", "//h3[text()='Python Backend Developer with Flask'][1]")
+
+#button on the footer
+FOOTERBTN = ("xpath", "//footer//div[text()='Full catalog']")
